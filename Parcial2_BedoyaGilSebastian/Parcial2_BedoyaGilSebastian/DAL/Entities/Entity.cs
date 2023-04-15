a45a8437054a97cf139a1c0f166254998c69e851
SEBASTIAN BEDOYA GIL <sebastianbedoya151125@correo.itm.edu.co>
2023-04-15 08:22:08 -0500

    creation of the Entity property

---
+++ Parcial2_BedoyaGilSebastian/Parcial2_BedoyaGilSebastian/DAL/Entities/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace Parcial2_BedoyaGilSebastian.DAL.Entities
+{
+    public class Entity
+    {
+        public  Guid Id { get; set; }
+    }
+}
---
+++ Parcial2_BedoyaGilSebastian/Parcial2_BedoyaGilSebastian/DAL/Entities/Tickets.cs
@@ -0,0 +1,6 @@
+﻿namespace Parcial2_BedoyaGilSebastian.DAL.Entities
+{
+    public class Tickets
+    {
+    }
+}
