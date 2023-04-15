9c99786e160bff0cae098747c578aee0ded543be
SEBASTIAN BEDOYA GIL <sebastianbedoya151125@correo.itm.edu.co>
2023-04-15 17:01:01 -0500

    Try Castsh to handle duplicate values

--- Parcial2_BedoyaGilSebastian/Parcial2_BedoyaGilSebastian/Controllers/TicketsController.cs
+++ Parcial2_BedoyaGilSebastian/Parcial2_BedoyaGilSebastian/Controllers/TicketsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -56,14 +57,31 @@ namespace Parcial2_BedoyaGilSebastian.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("UseDate,IsUsed,EntranceGate,Id,CreatedDate,ModifiedDate")] Tickets tickets)
+        public async Task<IActionResult> Create(Tickets tickets)
         {
             if (ModelState.IsValid)
             {
-                tickets.Id = Guid.NewGuid();
-                _context.Add(tickets);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                try
+                {
+                    _context.Update(tickets);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe un boleto con el mismo codigo.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
             }
             return View(tickets);
         }
