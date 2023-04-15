1a4f031c23a562abbe1197c3943eda76ee922bb5
SEBASTIAN BEDOYA GIL <sebastianbedoya151125@correo.itm.edu.co>
2023-04-15 07:43:58 -0500

    New proyect Parcial2_BedoyaGilSebastian_sat

---
+++ Parcial2_BedoyaGilSebastian/Parcial2_BedoyaGilSebastian/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Parcial2_BedoyaGilSebastian.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
