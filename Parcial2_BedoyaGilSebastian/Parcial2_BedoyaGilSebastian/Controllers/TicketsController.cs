a40d6664b856e2fa73af9cf7bbb5a257783c1ca4
SEBASTIAN BEDOYA GIL <sebastianbedoya151125@correo.itm.edu.co>
2023-04-15 15:29:35 -0500

    Index is updated bootstrap code is used

---
+++ Parcial2_BedoyaGilSebastian/Parcial2_BedoyaGilSebastian/Controllers/TicketsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Parcial2_BedoyaGilSebastian.DAL;
+using Parcial2_BedoyaGilSebastian.DAL.Entities;
+
+namespace Parcial2_BedoyaGilSebastian.Controllers
+{
+    public class TicketsController : Controller
+    {
+        private readonly DataBaseContextcs _context;
+
+        public TicketsController(DataBaseContextcs context)
+        {
+            _context = context;
+        }
+
+        // GET: Tickets
+        public async Task<IActionResult> Index()
+        {
+              return _context.Ticketes != null ? 
+                          View(await _context.Ticketes.ToListAsync()) :
+                          Problem("Entity set 'DataBaseContextcs.Ticketes'  is null.");
+        }
+
+        // GET: Tickets/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.Ticketes == null)
+            {
+                return NotFound();
+            }
+
+            var tickets = await _context.Ticketes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tickets == null)
+            {
+                return NotFound();
+            }
+
+            return View(tickets);
+        }
+
+        // GET: Tickets/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tickets/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UseDate,IsUsed,EntranceGate,Id,CreatedDate,ModifiedDate")] Tickets tickets)
+        {
+            if (ModelState.IsValid)
+            {
+                tickets.Id = Guid.NewGuid();
+                _context.Add(tickets);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tickets);
+        }
+
+        // GET: Tickets/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.Ticketes == null)
+            {
+                return NotFound();
+            }
+
+            var tickets = await _context.Ticketes.FindAsync(id);
+            if (tickets == null)
+            {
+                return NotFound();
+            }
+            return View(tickets);
+        }
+
+        // POST: Tickets/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("UseDate,IsUsed,EntranceGate,Id,CreatedDate,ModifiedDate")] Tickets tickets)
+        {
+            if (id != tickets.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tickets);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TicketsExists(tickets.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tickets);
+        }
+
+        // GET: Tickets/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _context.Ticketes == null)
+            {
+                return NotFound();
+            }
+
+            var tickets = await _context.Ticketes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tickets == null)
+            {
+                return NotFound();
+            }
+
+            return View(tickets);
+        }
+
+        // POST: Tickets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_context.Ticketes == null)
+            {
+                return Problem("Entity set 'DataBaseContextcs.Ticketes'  is null.");
+            }
+            var tickets = await _context.Ticketes.FindAsync(id);
+            if (tickets != null)
+            {
+                _context.Ticketes.Remove(tickets);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TicketsExists(Guid id)
+        {
+          return (_context.Ticketes?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
